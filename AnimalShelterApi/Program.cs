92f57da0ccbeeb8bc08b451d1ab66bade1546fcc
Hans Ellis <bullhorn@gmail.com>
2023-02-09 19:58:14 -0800

    add versioning package, configure associated builder services

--- AnimalShelterApi/Program.cs
+++ AnimalShelterApi/Program.cs
@@ -1,5 +1,6 @@
 using AnimalShelterApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Versioning;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,14 @@ builder.Services.AddDbContext<AnimalShelterApiContext>(
 							);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApiVersioning(opt => 
+																		{
+																			opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+																			opt.AssumeDefaultVersionWhenUnspecified = true;
+																			opt.ReportApiVersions = true;
+																			opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(), new HeaderApiVersionReader("x-api-version"), new MediaTypeApiVersionReader("x-api-version"));
+																		});
 
 var app = builder.Build();
 
