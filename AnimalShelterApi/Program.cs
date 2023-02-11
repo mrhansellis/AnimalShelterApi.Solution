0f751401d116a0b9301bfb2f9067db9b2d75945a
Hans Ellis <bullhorn@gmail.com>
2023-02-10 19:07:23 -0800

    configure program versioning for use with swagger

--- AnimalShelterApi/Program.cs
+++ AnimalShelterApi/Program.cs
@@ -1,6 +1,8 @@
 using AnimalShelterApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,8 +18,11 @@ builder.Services.AddDbContext<AnimalShelterApiContext>(
 									)
 								)
 							);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
 
 builder.Services.AddApiVersioning(opt => 
 																		{
@@ -27,18 +32,37 @@ builder.Services.AddApiVersioning(opt =>
 																			opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(), new HeaderApiVersionReader("x-api-version"), new MediaTypeApiVersionReader("x-api-version"));
 																		});
 
+builder.Services.AddVersionedApiExplorer(setup => 
+{
+		setup.GroupNameFormat = "'v'VVV";
+		setup.SubstituteApiVersionInUrl = true;
+});
+
 var app = builder.Build();
+
+var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
-	app.UseSwaggerUI();
+	app.UseSwaggerUI(options =>
+	{
+		foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions.Reverse())
+		{
+			options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+				description.GroupName.ToUpperInvariant());
+		}
+	});
 }
 else
 {
 	app.UseHttpsRedirection();
 }
+
+//app.UseRequestLocalization(options);
+app.UseStaticFiles();
+//app.UseMiddleware<LocalizerMiddleware>();
 
 app.UseAuthorization();
 
